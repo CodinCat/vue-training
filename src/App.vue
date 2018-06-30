9db41661b785476bdb5384d8a34701080312fa5f
CodinCat <a55951234@gmail.com>
2018-06-30 16:22:57 +0800

    Tweak style, use history mode

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,16 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+a {
+  text-decoration: none;
 }
 #nav {
   padding: 30px;
